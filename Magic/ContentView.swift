07efdd2db230deb8e3070ee71d3307ebcaf17238
Viraj Patel <virajpatel325@gmail.com>
2022-12-04 22:55:02 -0500

    Initial Commit

--- Magic/ContentView.swift
+++ Magic/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Charts
 
 struct ContentView: View {
     var body: some View {
@@ -13,6 +14,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
+            
             Text("Hello, world!")
         }
         .padding()
