a571d6a3596440876b2c031c821122477a76f743
Viraj Patel <virajpatel325@gmail.com>
2022-12-04 22:50:31 -0500

    Initial Commit

---
+++ Magic/MagicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicApp.swift
+//  Magic
+//
+//  Created by Viraj Patel on 12/4/22.
+//
+
+import SwiftUI
+
+@main
+struct MagicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
